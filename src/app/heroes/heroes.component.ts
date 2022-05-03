86d67a43b80e4a9f6110be25fd4ec4cd98048b1c
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2022-05-03 20:54:41 -0300

    feat: list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.interface';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,12 +8,14 @@ import { Hero } from '../hero.interface';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes: Hero[] = HEROES;
+  selectedHero?: Hero;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
